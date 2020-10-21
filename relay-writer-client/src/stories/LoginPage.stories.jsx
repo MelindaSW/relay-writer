62f11fee97213b16d1fc1696eac65feeee04e8a5
Melinda <sandstrommelinda@gmail.com>
2020-10-21 20:07:55 +0200

    Fix issue causing sass error warnings when starting storybook
    the scss preset for sass seems to have clashed with the create react app preset

---
+++ relay-writer-client/src/stories/LoginPage.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {LoginPage} from '../views';
+
+export default {
+    title: 'Views/Login',
+    component: LoginPage,
+    argTypes: {}
+};
+
+const Template = (args) => <LoginPage {...args}/>;
+
+export const Loginview = Template.bind({});
+Loginview.args = {};
