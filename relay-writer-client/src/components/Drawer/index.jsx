27bbdd55b7d67d85241f0c8755fdbc32f5f05265
Melinda <sandstrommelinda@gmail.com>
2020-12-01 17:15:12 +0100

    Add elevation on scroll for app bar

--- relay-writer-client/src/components/Drawer/index.jsx
+++ relay-writer-client/src/components/Drawer/index.jsx
@@ -6,7 +6,6 @@ import {
   Typography,
   Toolbar,
   ListItemText,
-  ListItemIcon,
   ListItem,
   List,
   IconButton,
@@ -15,11 +14,27 @@ import {
   Divider,
   AppBar
 } from '@material-ui/core'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import MenuIcon from '@material-ui/icons/Menu'
-import RadioButtonUncheckedOutlinedIcon from '@material-ui/icons/RadioButtonUncheckedOutlined'
 import { useTheme } from '@material-ui/core/styles'
 import { useStyles } from './drawer.styles'
 import './drawer.scss'
+
+function ElevationScroll(props) {
+  const { children } = props
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  })
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  })
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired
+}
 
 const ResponsiveDrawer = ({ title, drawerItems }) => {
   const classes = useStyles()
@@ -38,9 +53,6 @@ const ResponsiveDrawer = ({ title, drawerItems }) => {
         {drawerItems.map((item, index) => (
           <Link to={item.link} key={index}>
             <ListItem button>
-              <ListItemIcon>
-                <RadioButtonUncheckedOutlinedIcon style={{ fontSize: 15 }} />
-              </ListItemIcon>
               <ListItemText primary={item.text} />
             </ListItem>
           </Link>
@@ -52,22 +64,24 @@ const ResponsiveDrawer = ({ title, drawerItems }) => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            {title}
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              {title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
       <nav className={classes.drawer} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
@@ -100,22 +114,7 @@ const ResponsiveDrawer = ({ title, drawerItems }) => {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <FeatureRoutes />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
+        <FeatureRoutes /> {/* Create new story, continue story etc */}
       </main>
     </div>
   )
